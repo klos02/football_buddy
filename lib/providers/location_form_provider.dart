1df0249a44446478d95228e77861a6477afdf178
klos02 <bartoszklosinski02@gmail.com>
2025-04-25 16:52:36 +0200

    Add location functionality: implement AddLocationSheet and integrate with map screen

---
+++ lib/providers/location_form_provider.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:football_buddy/services/firestore_service.dart';
+
+class LocationFormProvider with ChangeNotifier {
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final FirestoreService _firestoreService = FirestoreService();
+  bool isLoading = false;
+
+  Future<void> addLocation(double lat, double lng, BuildContext context) async {
+    if (formKey.currentState!.validate()) {
+      isLoading = true;
+      notifyListeners();
+      try {
+        await _firestoreService.addLocation(
+          name: nameController.text,
+          latitude: lat,
+          longitude: lng,
+        );
+        Navigator.of(context).pop(); 
+      } on FirebaseException catch (e) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Error adding location: $e')));
+      }
+      finally {
+        isLoading = false;
+        notifyListeners();
+      }
+      
+  
+    }
+  }
+  void disposeFields() {
+    nameController.dispose();
+  }
+}
