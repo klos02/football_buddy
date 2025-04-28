5aee6158284bebe8cd2f0409223d0a9769195189
klos02 <bartoszklosinski02@gmail.com>
2025-04-29 00:39:30 +0200

    Implement location addition functionality with user details and selectable card options in AddLocationSheet

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -6,13 +6,24 @@ class FirestoreService {
   Future<void> addLocation({
     required String name,
     required GeoPoint location,
+    required String userId,
+    required String userName,
+    required String userPosition
     
   }) async {
     await _locations.add({
       'name': name,
       'location': location,
       'timestamp': FieldValue.serverTimestamp(),
+      'userId': userId,
+      'userName': userName,
+      'userPosition': userPosition,
     });
+  }
+
+  Future<String> getUserName(String userId) async {
+    final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    return userDoc.data()?['name'] ?? 'Unknown User';
   }
 
   
